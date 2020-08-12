2ccee1afb7621a0c05f94bf9e7f607e49678353f
Melikui <melikui@qq.com>
2020-08-12 23:10:58 +0800

    4 commit for 8.12

--- js/fnTime.js
+++ js/fnTime.js
@@ -1,32 +1,4 @@
 
-var oLiArray = document.getElementsByClassName('ele');
-var Timer = null;
-var oLiBg = oLiArray[oLiArray.length - 1];
-for (var i = 0; i < oLiArray.length - 1; i++) {
-    oLiArray[i].onmouseenter = function () {
-        startMove(oLiBg, this.offsetLeft);
-    }
-}
-
-function startMove(dom, target) {
-    clearInterval(Timer);
-    var a = 3;
-    var iSpeed = 0;
-    var u = 0.8;
-    Timer = setInterval(function () {
-        a = (target - dom.offsetLeft) / 5;
-        iSpeed += a;
-        iSpeed *= 0.8;
-        if (Math.abs(iSpeed) < 1 && (target - dom.offsetLeft) < 1) {
-            clearInterval(Timer);
-            dom.style.left = target + 'px';
-        } else {
-            dom.style.left = dom.offsetLeft + iSpeed + 'px';
-        }
-    }, 30);
-}
-
-
 // 首页时间展示区域函数----------------------------------------------------
 function fnTime() {
     window.setTimeout("fnTime()", 1000);
