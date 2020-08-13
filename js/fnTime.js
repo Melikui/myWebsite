b85543ebb121b174a150144d3cd93910a053c45e
Melikui <melikui@qq.com>
2020-08-13 13:02:05 +0800

    5 commit 完成了天气部分的内容

--- js/fnTime.js
+++ js/fnTime.js
@@ -19,3 +19,63 @@ fnTime();
 //     if (t < 10) return "0" + "" + t;
 //     else return t;
 // }
+// 发送 Ajax 请求，返回的结果是 Promise 对象
+function sendAjax(url) {
+    return new Promise((resolve, reject) => {
+        // 1.创建对象
+        const xhr = new XMLHttpRequest();
+        // 2.初始化
+        xhr.open('GET', url, true);
+        // 3.发送
+        xhr.send();
+        // 4.事件绑定
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    // 成功了
+                    resolve(xhr.responseText);
+                } else {
+                    // 如果失败
+                    reject(xhr.status);
+                }
+            }
+        }
+    });
+}
+// async 和 await 测试
+async function main() {
+    // 发送 Ajax 请求
+    let tianqi = await sendAjax('http://www.tianqiapi.com/api?version=v9&appid=23035354&appsecret=8YvlPNrz');
+    var txt = JSON.parse(tianqi);
+    function getEle(ele) {
+        return document.getElementsByClassName(ele)[0];
+    }
+    var tqdz = getEle('tqdz'),
+        tqtips = getEle('tqtips'),
+        tqkqsp = getEle('tqkqsp'),
+        tqwd = getEle('tqwd'),
+        tqwea = getEle('tqwea'),
+        tqwin = getEle('tqwin');
+    // 地址
+    tqdz.innerHTML = '中国 - 安徽 - ' + txt.aqi.city;
+    // 空气水平
+    tqkqsp.innerHTML = '空气水平: ' + txt.data[0].air_level;
+    // 建议
+    tqtips.innerHTML = txt.data[0].air_tips;
+    // 温度
+    tqwd.innerHTML = txt.data[0].tem + '℃';
+    // 天气
+    tqwea.innerHTML = txt.data[0].wea;
+    // 风向
+    tqwin.innerHTML = txt.data[0].win[0];
+    // 天气预报
+    var tqdays = document.getElementsByClassName('tqday');
+    var len = tqdays.length;
+    var str = '';
+    for (var i = 0; i < len; i++) {
+        str += '<p>' + txt.data[i].day + '</p>' + '<p>' + txt.data[0].tem + '℃' + '</p>' + '<p>' + txt.data[i].wea + '</p>' + '<p>' + txt.data[i].win[0] + '</p>' + '<p>' + txt.data[i].win_speed + '</p>';
+        tqdays[i].innerHTML = str;
+        str = '';
+    }
+}
+main();
